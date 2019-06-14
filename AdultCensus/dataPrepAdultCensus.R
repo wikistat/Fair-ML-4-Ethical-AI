7ea0972fe483d788b9dbc078b77ffb8f01ebb5a5
PhilBesse <besse@insa-toulouse.fr>
2019-06-14 12:35:28 +0200

    V0.64

--- AdultCensus/dataPrepAdultCensus.R
+++ AdultCensus/dataPrepAdultCensus.R
@@ -1,6 +1,6 @@
 dataPrepAdultCensus = function(path=""){
-  data = read.table(paste(path,"adultTrainTest.dat",sep=""),
-					sep=",",header=F,col.names=c("age", "workClass", "fnlwgt", "education", 
+  data = read.table(paste(path,"adultTrainTest.csv",sep=""),
+					sep=",",header=T,col.names=c("age", "workClass", "fnlwgt", "education", 
                     "educNum","mariStat", "occup", "relationship", "origEthn","sex",
                     "capitalGain", "capitalLoss", "hoursWeek","nativCountry", "income"),
 					fill=FALSE,strip.white=T)
